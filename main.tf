ccff5788dd800bbfe1fadd97ce29b0f2aaf4b1f2
shiverwaves <118776591+shiverwaves@users.noreply.github.com>
2023-10-12 10:21:59 -1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.0.0"
   backend "s3" {
-    bucket         = "tf-rm-st-bkt-923a07d"
+    bucket         = "tf-rm-st-bkt"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "tf-rm-st-tbl-923a07d"
+    dynamodb_table = "tf-rm-st-tb"
     encrypt        = true
   }
 }
