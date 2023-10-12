2b6526126748e90b89a7a92e9fd69aa8794e72b5
automation <automation@github_actions.com>
2023-10-12 20:21:01 +0000

    updating terraform remote_state

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.0.0"
   backend "s3" {
-    bucket         = "tf-rm-st-bkt"
+    bucket         = "tf-rm-st-bkt-923a07d"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "tf-rm-st-tbl"
+    dynamodb_table = "tf-rm-st-tbl-923a07d"
     encrypt        = true
   }
 }
